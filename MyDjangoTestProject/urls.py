101d18af1cbfc458ef2e536374b5e7da8cdfc741
lihuasheng1234 <lhsfromcia@gmail.com>
2020-05-26 22:21:37 +0800

    测试FileField字段的使用以及学习media的用法

--- MyDjangoTestProject/urls.py
+++ MyDjangoTestProject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from GetAPI import views as GetAPI_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/',GetAPI_views.page),
     path('app01/', include('app01.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
